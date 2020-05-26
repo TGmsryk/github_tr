86997d32915d17c665786a3f17271cfc175da7e9
masa <m.ohue616@gmail.com>
2020-05-26 17:29:01 +0900

    add a list identifier function

--- test.py
+++ test.py
@@ -5,3 +5,15 @@ import sys
 
 print(sys.version)
 
+def islist(l1, l2):
+    if l1 == l2:
+    	return True
+    else:
+    	return False
+
+l1 = [1, 2]
+s1 = {1,2}
+s1_list = list(s1)
+
+print(islist(l1, s1))
+print(islist(l1,s1_list))
